b25d450b2a3089820a54fb7e4f662b725c9c3699
Zudikhin <Zudikhin>
2022-04-18 20:16:10 +0300

    under final

--- js/main.js
+++ js/main.js
@@ -10,5 +10,32 @@ $(document).ready(function () {
         centerMode: false,
         variableWidth: true
     });
+
+    $('.talks_slider').slick({
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: false,
+        variableWidth: true,
+        prevArrow: $('.talks_btns_btn_prev'),
+        nextArrow: $('.talks_btns_btn_next')
+    });
+
+
+    $('.team_slider').slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: false,
+        variableWidth: true
+    });
+
+    $(".faq_block_list_item").click(function() {
+        $(this).toggleClass("active");
+    });
     
 });
