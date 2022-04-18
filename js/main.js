6d67ccd157744aff022480807b7000a08beec5af
Zudikhin <Zudikhin>
2022-04-18 11:34:53 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    "use strict";
+
+    $('.media_slider').slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: false,
+        variableWidth: true
+    });
+    
+});
