34648da9bb297ac7fc85fd9a174fa6fbf328a4bf
Zudikhin <Zudikhin>
2022-04-19 17:11:45 +0300

    almost

--- js/main.js
+++ js/main.js
@@ -23,7 +23,6 @@ $(document).ready(function () {
         nextArrow: $('.talks_btns_btn_next')
     });
 
-
     $('.team_slider').slick({
         dots: false,
         arrows: false,
@@ -34,8 +33,52 @@ $(document).ready(function () {
         variableWidth: true
     });
 
+    $(".header ul li").click(function() {
+        var attr = $(this).attr("data-target");
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(`#${attr}`).offset().top
+        }, 2000);
+    });
+
     $(".faq_block_list_item").click(function() {
         $(this).toggleClass("active");
+    });
+
+    $(".faq_cooperation_call").click(function() {
+        $(".form").addClass("active");
+        $(".modal_back").addClass("active");
+    }); 
+
+    $(".application_btn").click(function() {
+        $(".form").addClass("active");
+        $(".modal_back").addClass("active");
+    });
+
+    $(".header_desktop_form").click(function() {
+        $(".form").addClass("active");
+        $(".modal_back").addClass("active");
+    }); 
+
+    $(".form_close").click(function() {
+        $(".form").removeClass("active");
+        $(".modal_back").removeClass("active");
+    });
+
+    $(".modal_back").click(function() {
+        $(".form").removeClass("active");
+        $(".thank").removeClass("active");
+        $(this).removeClass("active");
+    });
+
+    $(".thank_close").click(function() {
+        $(".thank").removeClass("active");
+        $(".modal_back").removeClass("active");
+    });
+
+    $("#form").submit(function( event ) {
+        $(".form").removeClass('active');
+        $(".thank").addClass('active');
+        event.preventDefault();
     });
     
 });
